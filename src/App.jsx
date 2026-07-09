2272e2c40f0c94093ad1ab462e8d673e88bca5bb
Kaushik Chakraborty <master.trainer049@gmail.com>
2026-07-09 19:12:34 +0530

    Added scroll-driven SVG path animation

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ import Schedule from './components/Schedule';
 import Prizes from './components/Prizes';
 import Sponsors from './components/Sponsors';
 import FAQs from './components/FAQs';
-import WanderingLine from './components/WanderingLine';
+import ScrollPath from './components/ScrollPath';
 import './index.css';
 
 function App() {
   return (
     <div className="app-container">
-      <WanderingLine />
+      <ScrollPath />
       <nav className="navbar">
         <div className="logo-container">
           <img src="/DevAxe.png" alt="DevAxe Hacks Logo" className="logo-img" />
